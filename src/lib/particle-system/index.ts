0815502a0cd58db305c25f7a668c135c693a768e
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2026-02-09 09:04:42 +0100

    scaffold svelte

---
+++ src/lib/particle-system/index.ts
@@ -0,0 +1,5 @@
+import { Simulation } from './core';
+import { EmittedPixels, Forces, Frontiers, ImageEmitter, Trails } from './extras';
+import { P5Renderer } from './renderer';
+
+export { Simulation, EmittedPixels, Forces, Frontiers, ImageEmitter, Trails, P5Renderer };
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,36 @@
-import { defineConfig } from "vite";
-import { fileURLToPath } from "node:url";
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vitest/config';
+import { playwright } from '@vitest/browser-playwright';
+import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      $particles: fileURLToPath(new URL("./src/core", import.meta.url)),
-    },
-  },
+	plugins: [tailwindcss(), sveltekit()],
+	test: {
+		expect: { requireAssertions: true },
+		projects: [
+			{
+				extends: './vite.config.ts',
+				test: {
+					name: 'client',
+					browser: {
+						enabled: true,
+						provider: playwright(),
+						instances: [{ browser: 'chromium', headless: true }]
+					},
+					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+					exclude: ['src/lib/server/**']
+				}
+			},
+
+			{
+				extends: './vite.config.ts',
+				test: {
+					name: 'server',
+					environment: 'node',
+					include: ['src/**/*.{test,spec}.{js,ts}'],
+					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+				}
+			}
+		]
+	}
 });
